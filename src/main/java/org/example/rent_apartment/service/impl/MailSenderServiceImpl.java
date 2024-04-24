0bf065b51439306a915eb0e709231b3a043373b7
Babiy-Lesha <babiilesha@gmail.com>
2024-04-24 23:48:46 +0500

    защита

--- src/main/java/org/example/rent_apartment/service/impl/MailSenderServiceImpl.java
+++ src/main/java/org/example/rent_apartment/service/impl/MailSenderServiceImpl.java
@@ -1,10 +1,15 @@
 package org.example.rent_apartment.service.impl;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
 import lombok.RequiredArgsConstructor;
 import org.example.rent_apartment.service.MailSenderService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 
@@ -29,6 +34,21 @@ public class MailSenderServiceImpl implements MailSenderService {
         simpleMailMessage.setSubject(subject);
         simpleMailMessage.setText(massage);
 
-        javaMailSender.send(simpleMailMessage);
+        try {
+            javaMailSender.send(simpleMailMessage);
+        } catch (MailException ex) {
+            ((JavaMailSenderImpl) javaMailSender).getJavaMailProperties().setProperty("mail.smtp.quitwait", "true");
+            Session session = ((JavaMailSenderImpl) javaMailSender).getSession();
+            Transport transport;
+            try {
+                transport = session.getTransport();
+                // Закрываем соединение
+                if (transport != null) {
+                    transport.close();
+                }
+            } catch (MessagingException ignored) {
+
+            }
+        }
     }
 }
